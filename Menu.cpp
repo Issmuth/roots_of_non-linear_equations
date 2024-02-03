44489a3498fc3434ac19e0883dfeb0ea22a563f7
issmuth <ismael.mdev@gmail.com>
2024-02-03 21:23:19 +0300

    header file and menu design

---
+++ Menu.cpp
@@ -0,0 +1,16 @@
+#include "Root.h" //including our header file
+
+using namespace std;
+
+void menu(){
+    cout << "--------- Welcome to the Root finder --------------\n";
+    cout << "What type of function are you using?\n";
+    cout << "1. Polynomial function";
+    cout << "2. Exponential function";
+    cout << "3. Trignometric function";
+    cout << "4. Hyperbolic function";
+}
+
+int main(){
+    menu();
+}
---
+++ Root.h
@@ -0,0 +1,15 @@
+#ifndef ROOT_H //include gaurding the header file
+#define ROOT_H
+
+/*Define the error tolerance value
+so the bisection iteration will stop at this point*/
+#define Error_Tolerance 0.0001
+
+//including necessary libraries
+#include <iostream>
+#include <cmath>
+#include <string>
+
+// listing function prototypes
+float bisection(float **guess, );
+#endif
